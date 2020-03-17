0066334a18f65bada6f2fc496184b9728eb0ef4d
Veprev Alexey <Alexey.v.veprev@gmail.com>
2020-03-17 19:38:34 +0200

    update sdk version

--- packages/mail/src/classes/virtru-service.js
+++ packages/mail/src/classes/virtru-service.js
@@ -1,12 +1,13 @@
 const Virtru = require('virtru-sdk');
 
-const encryptAttachment = async (attachmentBuffer, authData, recipients) => {
+const encryptAttachment = async (attachmentBuffer, fileName, authData, recipients) => {
     const client = new Virtru.Client(authData);
     const policy = new Virtru.PolicyBuilder()
         .build();
     const encryptParams = new Virtru.EncryptParamsBuilder()
         .withBufferSource(attachmentBuffer)
         .withUsersWithAccess(recipients)
+        .withDisplayFilename(fileName)
         .withPolicy(policy)
         .build();
     const ct = await client.encrypt(encryptParams);
@@ -16,7 +17,7 @@ const encryptAttachment = async (attachmentBuffer, authData, recipients) => {
 const encryptAttachments = async (attachments, virtruAuth, sharedUserEmails) => {
     const result = attachments.map(async (attachment) => {
         const attachmentBuffer = Buffer.from(attachment.content, 'base64');
-        const encryptedBase64String = await encryptAttachment(attachmentBuffer, virtruAuth, sharedUserEmails);
+        const encryptedBase64String = await encryptAttachment(attachmentBuffer, attachment.filename, virtruAuth, sharedUserEmails);
         return {
             content: encryptedBase64String,
             filename: `${attachment.filename}.tdf.html`,
