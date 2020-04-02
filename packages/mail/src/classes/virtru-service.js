54b3c9190871da30dec77c64d849670780321727
Veprev Alexey <Alexey.v.veprev@gmail.com>
2020-04-02 15:30:57 +0300

    add encrypt email method

--- packages/mail/src/classes/virtru-service.js
+++ packages/mail/src/classes/virtru-service.js
@@ -1,36 +1,36 @@
-const Virtru = require('virtru-sdk');
+// const Virtru = require('virtru-sdk');
 const SecureService = require('secure-lib').SecureService;
 
 const secureService = SecureService.setup();
 
-const encryptAttachment = async (attachmentBuffer, fileName, authData, recipients) => {
-    const client = new Virtru.Client(authData);
-    const policy = new Virtru.PolicyBuilder()
-        .build();
-    const encryptParams = new Virtru.EncryptParamsBuilder()
-        .withBufferSource(attachmentBuffer)
-        .withUsersWithAccess(recipients)
-        .withDisplayFilename(fileName)
-        .withPolicy(policy)
-        .build();
-    const ct = await client.encrypt(encryptParams);
-    const buffer = await ct.toBuffer();
-    return buffer.toString('base64');
-};
-const encryptAttachments = async (attachments, virtruAuth, sharedUserEmails) => {
-    const result = attachments.map(async (attachment) => {
-        const attachmentBuffer = Buffer.from(attachment.content, 'base64');
-        const encryptedBase64String = await encryptAttachment(attachmentBuffer, attachment.filename, virtruAuth, sharedUserEmails);
-        return {
-            content: encryptedBase64String,
-            filename: `${attachment.filename}.tdf.html`,
-            type: 'text/html',
-            disposition: 'attachment',
-            contentId: `${attachment.contentId}_tdf_html`
-        };
-    });
-    return Promise.all(result);
-};
+// const encryptAttachment = async (attachmentBuffer, fileName, authData, recipients) => {
+//     const client = new Virtru.Client(authData);
+//     const policy = new Virtru.PolicyBuilder()
+//         .build();
+//     const encryptParams = new Virtru.EncryptParamsBuilder()
+//         .withBufferSource(attachmentBuffer)
+//         .withUsersWithAccess(recipients)
+//         .withDisplayFilename(fileName)
+//         .withPolicy(policy)
+//         .build();
+//     const ct = await client.encrypt(encryptParams);
+//     const buffer = await ct.toBuffer();
+//     return buffer.toString('base64');
+// };
+// const encryptAttachments = async (attachments, virtruAuth, sharedUserEmails) => {
+//     const result = attachments.map(async (attachment) => {
+//         const attachmentBuffer = Buffer.from(attachment.content, 'base64');
+//         const encryptedBase64String = await encryptAttachment(attachmentBuffer, attachment.filename, virtruAuth, sharedUserEmails);
+//         return {
+//             content: encryptedBase64String,
+//             filename: `${attachment.filename}.tdf.html`,
+//             type: 'text/html',
+//             disposition: 'attachment',
+//             contentId: `${attachment.contentId}_tdf_html`
+//         };
+//     });
+//     return Promise.all(result);
+// };
 
 const encryptEmail = async (owner, subject, recipients, message, attachments) => {
     const attachmentPromises = [];
